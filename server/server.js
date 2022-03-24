c14ba99031e12aba4ba931fed88e172a5b1639ab
Lisa Lee <lisawlee04@gmail.com>
2022-03-24 00:27:06 -0700

    POST request works but need to combine date and time params.

--- server/server.js
+++ server/server.js
@@ -9,12 +9,12 @@ const PORT = 5005;
 app.use(cors());
 app.use(express.json());
 
-//creates an endpoint for the route /
+// creates an endpoint for the route /
 app.get('/', (req, res) => {
     res.json({ message: 'Hello from My ExpressJS' });
 });
 
-//GET request for species table but endpoint is /animals
+// GET request for species table but endpoint is /animals
 app.get('/animals', cors(), async (req, res) => {
     // res.json(animals);
     try{
@@ -25,7 +25,7 @@ app.get('/animals', cors(), async (req, res) => {
     }
 });
 
-//GET request for individuals table
+// GET request for individuals table
 app.get('/individuals', cors(), async (req, res) => {
     // res.json(animals);
     try{
@@ -36,7 +36,7 @@ app.get('/individuals', cors(), async (req, res) => {
     }
 });
 
-//GET request for sightings table
+// GET request for sightings table
 app.get('/sightings', cors(), async (req, res) => {
     // res.json(animals);
     try{
@@ -47,18 +47,31 @@ app.get('/sightings', cors(), async (req, res) => {
     }
 });
 
+// POST request for sightings list
+// need to find out how to combine date & time
+app.post('/sightings', cors(), async (req, res) => {
+    const newSighting = { 
+        date: req.body.date, 
+        time: req.body.time, 
+        location: req.body.location, 
+        isHealthy: req.body.isHealthy, 
+        email: req.body.email, 
+        individualsId: req.body.individualsId
+    }
 
-// //POST request for individuals table
-// app.post('/api/animals', cors(), async (req, res) => {
-//     const newSighting = { date: req.body.date, time: req.body.time, individual: req.body.individual, location: req.body.location, isHealthy: req.body.isHealthy, email: req.body.email }
-//     console.log("this is the post", [newSighting.date, newSighting.time, newSighting.individual, newSighting.location, newSighting.isHealthy, newSighting.email ]);
-//     const result = await db.query(
-//         'INSERT INTO individuals (date, time, individual, location, isHealthy, email) VALUES($1, $2, $3, $4, $5, $6) RETURNING *',
-//         [newSighting.date, newSighting.time, newSighting.individual, newSighting.location, newSighting.isHealthy, newSighting.email]
-//     );
-//     console.log(result.rows[0]);
-//     res.json(result.rows[0]);
-// });
+    // console.log("this is the post", [newSighting.date, newSighting.time, newSighting.location, newSighting.isHealthy, newSighting.email, newSighting.recordCreationTimestamp ]);
+    const datetime = new Date().toISOString();
+    const creationTime = new Date().toISOString();
+    const result = await db.query(
+        // INSERT INTO needs to match db
+        // individuals_id should be individual_id ideally
+        'INSERT INTO sightings (date_time, location, is_healthy, email, individuals_id, record_creation_timestamp) VALUES($1, $2, $3, $4, $5, $6) RETURNING *',
+        // [datetime, "California", true, "sighting@sighting.com", 1, creationTime]
+        [datetime, newSighting.location, newSighting.isHealthy, newSighting.email, newSighting.individualsId, creationTime]
+    );
+    console.log(result);
+    res.json(result);
+});
 
 // console.log that your server is up and running
 app.listen(PORT, () => {
